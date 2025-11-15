7a813327a0a66c3f1485f499e1ae472ee1df19b0
benitaekpu <boe2@njit.edu>
2025-11-15 14:57:11 -0500

    Add files via upload

--- Mental Health Log.cpp
+++ Mental Health Log.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <ctime>
 #include <string>
+#include <limits>
+#include <vector>
+#include <cstdlib>   // rand
+
+// generate random encouragement case 5
+std::string getRandomEncouragement(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        return "Error: Could not open encouragement file.";
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+
+    while (std::getline(file, line)) {
+        if (!line.empty())
+            lines.push_back(line);
+    }
+
+    if (lines.empty()) {
+        return "No encouragements found.";
+    }
+
+    // Seed random once using time
+    std::srand(std::time(0));
+
+    int index = std::rand() % lines.size();
+    return lines[index];
+}
+
 
 
 int main() {
@@ -19,20 +50,30 @@ int main() {
             << "7. Exit\n";
 
         std::cin >> choice;
-        std::cin.ignore(); // clear leftover newline
-
-        std::string todays_entry;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        // file to store entries in
         std::string output_file = "log.txt";
+        // file where encouragements are stored in
+        std::string msg = getRandomEncouragement("encouragements.txt");
 
-        switch (choice) {
+
+   
+
+    switch (choice) {
+
         case 1: {
+        
+
+            // ==== GET ENTRY ====
+            std::string todays_entry;
             std::cout << "Write today's entry:\n> ";
             std::getline(std::cin, todays_entry);
 
+            // ==== WRITE TO FILE ====
             std::ofstream os(output_file, std::ios::app);
-
             if (os.is_open()) {
-                os << "Entry: " << todays_entry << "\n";
+
+                 os << "Entry: " << todays_entry << "\n\n";
                 os.close();
                 std::cout << "Entry saved.\n";
             }
@@ -41,7 +82,9 @@ int main() {
             }
             break;
         }
-
+        case 5:
+            std::cout << "\n Encouragement: " << msg << "\n";
+            break;
         case 7:
             std::cout << "Goodbye!\n";
             return 0;
